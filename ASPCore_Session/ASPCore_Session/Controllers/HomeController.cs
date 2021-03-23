29909c09983bf7d1501a9f382d53f0a22855e414
Mahboob Alam <malam@dc.nexuslink.com>
2021-03-23 11:19:33 +0530

    First Commit

---
+++ ASPCore_Session/ASPCore_Session/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using ASPCore_Session.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+
+namespace ASPCore_Session.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var user = new userInfo() { UserId = 1, UserName = "Nexus" };
+            HttpContext.Session.SetString("sessionUser", JsonConvert.SerializeObject(user));
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ASPCore_Session/ASPCore_Session/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+
+namespace ASPCore_Session.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            var user = HttpContext.Session.GetString("sessionUser");
+            var userinfo = JsonConvert.DeserializeObject<userInfo>(HttpContext.Session.GetString("sessionUser"));
+            return View(userinfo);
+        }
+    }
+}
